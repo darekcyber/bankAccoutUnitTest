bee27bd72dbe7c30a2a00718433cb467c90606f5
darekcyber <darrcyber@gmail.com>
2021-08-23 19:00:54 +0200

    Deposit_InvalidAmount_UpdateBalance

--- BankAccountTest/UnitTest1.cs
+++ BankAccountTest/UnitTest1.cs
@@ -22,8 +22,16 @@ namespace BankAccountTest
             account.Deposit(depositValue);
 
             Assert.AreEqual(account.Balance, expectedBalance);
+        }
 
+        [Test]
+        public void Deposit_InvalidAmount_UpdateBalance()
+        {
+            double currentBalance = 11.45;
+            double depositValue = -1;
+            BankAccount account = new BankAccount(currentBalance);
 
+            Assert.Throws<ArgumentOutOfRangeException>(() => account.Deposit(depositValue));
         }
 
 
