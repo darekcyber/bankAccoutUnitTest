2e75cf17c5ede8d08b411c82b802437bc8eb9300
darekcyber <darrcyber@gmail.com>
2021-08-23 19:41:18 +0200

    Deposit_ValidAmount_UpdateBalance

--- BankAccountTest/UnitTest1.cs
+++ BankAccountTest/UnitTest1.cs
@@ -14,14 +14,14 @@ namespace BankAccountTest
         [Test]
         public void Deposit_ValidAmount_UpdateBalance()
         {
-            double currentBalance = 11.45;
+            double currentBalance = 11.49;
             double depositValue = 6.25;
-            double expectedBalance = 17.70;
+            double expectedBalance = 17.74;
             BankAccount account = new BankAccount(currentBalance);
 
             account.Deposit(depositValue);
 
-            Assert.AreEqual(account.Balance, expectedBalance);
+            Assert.AreEqual(account.Balance, expectedBalance, 0.01);
         }
 
         [Test]
@@ -32,6 +32,20 @@ namespace BankAccountTest
             BankAccount account = new BankAccount(currentBalance);
 
             Assert.Throws<ArgumentOutOfRangeException>(() => account.Deposit(depositValue));
+        }
+
+        [Test]
+        public void Withdraw_ValidAmount_UpdateBalance()
+        {
+            double currentBalance = 11.45;
+            double withdrawValue = 6.55;
+            double expectedBalance = 4.90;
+
+            BankAccount account = new BankAccount(currentBalance);
+
+            account.Withdraw(withdrawValue);
+
+            Assert.AreEqual(expectedBalance, account.Balance, 0.01);
         }
 
 
